ce81b52f6eff1cd2e70c5d2082d8bc64880363ea
Taha Fayyaz <taha.fayyaz1@gmail.com>
2024-10-12 23:30:13 +0500

    Search (Task 1, Subtask 1) Completed

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
-
+from django.http  import HttpResponse,  HttpResponseRedirect
+from django import forms
 from . import util
+from django.urls import reverse
 
 
 def index(request):
+    if request.method== "POST":
+        search_form= SearchForm(request.POST)
+
+        if search_form.is_valid():
+            search = search_form.cleaned_data["search"]
+            return HttpResponseRedirect(reverse("encyclopedia:display_content", args = [search]))
+        
+        else:
+            return HttpResponse ("Invalid")
+
+
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "search_form": SearchForm()
     })
+
 
 
 
@@ -19,3 +33,8 @@ def display_content (request, title):
         return render (request, "encyclopedia/error.html", {"title": title})
     else:
         return render (request, "encyclopedia/content.html", context)
+    
+
+
+class SearchForm(forms.Form):
+    search=forms.CharField(label="search")
